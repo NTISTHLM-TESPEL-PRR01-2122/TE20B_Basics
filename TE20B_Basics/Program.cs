615225d97c88db83168fb75139a33b21bcf54509
Mikael Bergström <krank23@gmail.com>
2021-08-31 13:21:19 +0200

    Initial commit

---
+++ TE20B_Basics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Please write your name: ");
+
+string name;
+
+name = "Micke";
+
+name = Console.ReadLine();
+
+Console.WriteLine("Hej, " + name + "!");
+Console.WriteLine($"Hej, {name}! Ursch vad du är ful.");
+
+Console.ReadLine();
